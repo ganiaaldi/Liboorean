eb05afa852148e0b768e3a0df6ab220eb015d3e1
ganiaaldi <mrgad98@gmail.com>
2019-11-28 08:42:41 +0700

    Add interface Change toolbar tittle & edit bottom nav menu

---
+++ app/src/main/java/com/aldi/liboorean/Interface/ChangeToolbarTitle.kt
@@ -0,0 +1,12 @@
+package com.aldi.liboorean.Interface
+
+import android.graphics.drawable.Drawable
+import android.view.View
+
+interface ChangeToolbarTitle{
+
+    fun updateTitle(title: String)
+    fun toolbarAction(onClickListener: View.OnClickListener)
+    fun updateNavIcon(drawable: Drawable)
+    fun showToolbar(show: Boolean)
+}
