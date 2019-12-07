0928da3a99c2bced27d0b2a501d7d7480e85e43f
ganiaaldi <mrgad98@gmail.com>
2019-12-07 15:20:48 +0700

    Fixed RecyclerView Promo

--- app/src/main/java/com/aldi/liboorean/Menu/FirstMenuFragment.kt
+++ app/src/main/java/com/aldi/liboorean/Menu/FirstMenuFragment.kt
@@ -7,19 +7,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.aldi.liboorean.Adapter.PromoAdapter
 import com.aldi.liboorean.Interface.ChangeToolbarTitle
+import com.aldi.liboorean.Model.Promo
 
 import com.aldi.liboorean.R
 import kotlinx.android.synthetic.main.fragment_first_menu.*
 
 class FirstMenuFragment : Fragment() {
 
+    private val promoData = arrayListOf(
+        Promo("Raising Arizona", "aa", R.drawable.jungle),
+        Promo("Vampire's Kiss", "bb", R.drawable.hotel_banner),
+        Promo("Con Air", "cc", R.drawable.bk)
+    )
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first_menu, container, false)
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -27,7 +37,9 @@ class FirstMenuFragment : Fragment() {
         (activity as ChangeToolbarTitle).showToolbar(true)
         (activity as ChangeToolbarTitle).updateTitle("Beranda")
 
-       val rvPromoo = rvPromo
-        rvPromoo.setHasFixedSize(true)
+        rvPromo.apply {
+            layoutManager = LinearLayoutManager(activity, LinearLayout.HORIZONTAL, false)
+            adapter = PromoAdapter(promoData)
+        }
     }
 }
